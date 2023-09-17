ad5e807258e6bc99f9793d2e6077e5eee5b97984
sithanga rashmika <87372439+IT21041716@users.noreply.github.com>
2023-09-17 12:41:55 +0530

    yser repo change

--- src/main/java/com/example/CodeFest/repo/UserRepository.java
+++ src/main/java/com/example/CodeFest/repo/UserRepository.java
@@ -1,9 +1,7 @@
 package com.example.CodeFest.repo;
 
-import java.util.List;
-
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
+
 
 import com.example.CodeFest.model.User;
 
