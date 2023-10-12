b2158f9de51056a26bd10536e0325803d1de26b6
Hiruna Lalanga <IT21049590@my.sliit.lk>
2023-10-12 10:25:10 +0530

    did some changes in css file

--- front_end/src/IT21049590/components/QuestionCard.jsx
+++ front_end/src/IT21049590/components/QuestionCard.jsx
@@ -55,9 +55,20 @@ const QuestionCard = ({ question }) => {
           <Typography color="text.secondary" paragraph>
             {question.content}
           </Typography>
+          <center>
+            {question.image && (
+              <CardMedia
+                sx={{ width: 800, height: 500, marginBottom: 2 }}
+                component="img"
+                image={`../../../public/hirunaUploadsQuestion Images/${question.image}`}
+                alt={question.user.firstName}
+              />
+            )}
+          </center>
           <Button
             onClick={() => handleClickOpen(question.id)}
             variant="outlined"
+            sx={{ marginLeft: 50 }}
             startIcon={<ForumIcon />}
           >
             View Answers
@@ -65,6 +76,7 @@ const QuestionCard = ({ question }) => {
           <Button
             onClick={() => deleteQuestion(question.id)}
             variant="outlined"
+            sx={{ marginLeft: 5 }}
             startIcon={<DeleteIcon />}
           >
             Delete
@@ -74,37 +86,40 @@ const QuestionCard = ({ question }) => {
       <br />
 
       <Dialog open={open} onClose={handleClose}>
-        <DialogTitle>Answers</DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-            <Grid container spacing={2}>
-              {answers.map((ansers, index) => (
-                <Grid item xs={12} key={index}>
-                  <Avatar sx={{ width: 100, height: 100, marginBottom: 2 }}>
-                    <CardMedia
-                      component="img"
-                      height="100"
-                      image={`../../../public/hirunaUploadsUserImages/${ansers.user.image}`}
-                      alt={ansers.user.firstName}
-                    />
-                  </Avatar>
-                  <Typography variant="subtitle1">
-                    <strong>User</strong> {ansers.user.firstName}
-                  </Typography>
-                  <Typography variant="body1">
-                    <strong>Answer:</strong> {ansers.content}
-                  </Typography>
-                </Grid>
-              ))}
-            </Grid>
-          </DialogContentText>
-          {/* You can add your answer form or display answers here */}
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose} color="primary">
-            Close
-          </Button>
-        </DialogActions>
+        <div style={{ width: 400, padding: 16 }}>
+          <DialogTitle>Answers</DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              <Grid container spacing={2}>
+                {answers.map((ansers, index) => (
+                  <Grid item xs={12} key={index}>
+                    <Avatar sx={{ width: 50, height: 50, marginBottom: 2 }}>
+                      <CardMedia
+                        component="img"
+                        height="50"
+                        image={`../../../public/hirunaUploadsUserImages/${ansers.user.image}`}
+                        alt={ansers.user.firstName}
+                      />
+                    </Avatar>
+                    <Typography variant="subtitle1">
+                      <strong>User : </strong> {ansers.user.firstName}{" "}
+                      {ansers.user.lastName}
+                    </Typography>
+                    <Typography variant="body1">
+                      <strong>Answer : </strong> {ansers.content}
+                    </Typography>
+                  </Grid>
+                ))}
+              </Grid>
+            </DialogContentText>
+            {/* You can add your answer form or display answers here */}
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose} color="primary">
+              Close
+            </Button>
+          </DialogActions>
+        </div>
       </Dialog>
     </>
   );
--- src/main/java/com/example/CodeFest/services/UserService.java
+++ src/main/java/com/example/CodeFest/services/UserService.java
@@ -56,9 +56,7 @@ public class UserService {
     public Optional<User> getOne(String id) {
         return repository.findById(id);
     }
-     public User getOne1(String id) {
-        return repository.findById(id);
-    }
+     
 
     public String login(User user) {
         User existingUser = repository.findByEmail(user.getEmail());
